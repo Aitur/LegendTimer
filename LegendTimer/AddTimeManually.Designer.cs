04438224d61599bf7ada2e81be3fcac2ce8e7671
Kikinomini <nkiwatrowski@web.de>
2018-10-19 16:23:57 +0200

    Chrissys changes recovery

--- LegendTimer/AddTimeManually.Designer.cs
+++ LegendTimer/AddTimeManually.Designer.cs
@@ -74,7 +74,6 @@ namespace LegendTimer
             this.button1.TabIndex = 3;
             this.button1.Text = "Bestätigen";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.TimeNachtragen);
             // 
             // dateTimePicker
             // 
